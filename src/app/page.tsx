71a6e890eb8c8f89068e05351cbf56d2efb91a01
francesjgonzales <francesj7837@gmail.com>
2023-12-27 00:31:27 -0700

    adding content and style in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,17 @@ export default async function Home() {
   return (
     <main>
       <Slider />
-      <section>Services</section>
-      <section>Projects</section>
-      <section>News and Events</section>
-      <section>Contact us and Sign up Newsletter</section>
+      <section className="articles">
+        Services
+        <p>
+          SafeBuild has been rated by the Construction Authority as a Grade A1
+          contractor in the Contractors Registry System, and also as a Class 1
+          General Builder under the Licensing of Builders.
+        </p>
+      </section>
+      <section className="articles">Projects</section>
+      <section className="articles">News and Events</section>
+      <section className="articles">Contact us and Sign up Newsletter</section>
     </main>
   );
 }
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,17 +2,14 @@ import React from "react";
 
 export const Slider = () => {
   return (
-    <div>
-      <div className="slider bg-top bg-cover bg-[url('/assets/hero-img.jpg')]">
-        <h1 className=" pt-5">
-          SafeBuild offers comprehensive services within the construction
-          industry and is recognized as one of the leading companies in the
-          field.
-        </h1>
-        <p className="text-sm">
-          <button className="slider__button">Learn More</button>
-        </p>
-      </div>
+    <div className="slider bg-top bg-cover bg-[url('/assets/hero-img.jpg')]">
+      <h1 className=" pt-5">
+        SafeBuild offers comprehensive services within the construction industry
+        and is recognized as one of the leading companies in the field.
+      </h1>
+      <p className="text-sm">
+        <button className="slider__button">Learn More</button>
+      </p>
     </div>
   );
 };
