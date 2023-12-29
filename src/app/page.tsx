7b8a60a0ed053678c07b6b777e2687ac99b8c2d3
francesjgonzales <francesj7837@gmail.com>
2023-12-29 13:52:31 -0700

    adding style to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -254,7 +254,7 @@ export default async function Home() {
         </div>
       </section>
       <section className="one-col bg-[rgb(204,204,204)]">
-        <div className="two-col">
+        <div className="two-col flex items-start">
           <div className="flex">
             <div>
               <Image
@@ -280,10 +280,10 @@ export default async function Home() {
                   <div className="mt-2">
                     <input
                       type="text"
-                      name="first-name"
-                      id="first-name"
+                      name="full-name"
+                      id="full-name"
                       autoComplete="given-name"
-                      className="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block rounded-md border-0 py-1.5 w-96 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -301,18 +301,23 @@ export default async function Home() {
                       name="email"
                       type="email"
                       autoComplete="email"
-                      className="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block rounded-md border-0 py-1.5 w-96 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
               </div>
-              <div className="mt-6 flex items-center justify-end gap-x-6">
+              <div className="mt-6 flex items-center justify-start gap-x-6">
                 <button
                   type="button"
                   className="text-sm font-semibold leading-6 text-gray-900"
                 >
                   Cancel
                 </button>
+                <p className="text-sm pt-10">
+                  <button className="slider__button text-[#D21448]">
+                    Submit
+                  </button>
+                </p>
                 <button
                   type="submit"
                   className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -338,11 +343,11 @@ export default async function Home() {
                 14 Kung Chong Road #08-01 Lum Chang Building Singapore 159150
               </p>
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div className="sm:col-span-3">
+                <div className="sm:col-span-3 pb-8">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8070570349346!2d103.8112565!3d1.2900376000000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da1a2d46aeeed3%3A0xb71f90076c9830b7!2s14%20Kung%20Chong%20Rd%2C%20Singapore%20159150!5e0!3m2!1sen!2sca!4v1703828476719!5m2!1sen!2sca"
-                    width="600"
-                    height="450"
+                    width="350"
+                    height="250"
                   ></iframe>
                 </div>
               </div>
