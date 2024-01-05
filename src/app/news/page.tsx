28dab8453ddb0d636fd0e42122ea2ae433fac746
francesjgonzales <francesj7837@gmail.com>
2024-01-04 20:52:38 -0700

    fixing database_url error

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className="articles">
       <div className="slider__medium bg-top bg-cover bg-[url('/assets/proj-hero-img.jpg')]">
-        <p className="text-3xl ">News and Events</p>
+        <p className="text-3xl ">NNews and Events</p>
       </div>
       <div className="articles"></div>
     </div>
