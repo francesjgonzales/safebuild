da32444661f840b8b6555f77dd0b4201316fa120
francesjgonzales <francesj7837@gmail.com>
2024-01-06 17:16:09 -0700

    moved grafbase.config in root folde

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -7,7 +7,7 @@ import { graph, config } from "@grafbase/sdk";
 const g = graph.Standalone();
 
 const user = g.type("User", {
-  user_name: g.string().length({ min: 2, max: 20 }),
+  name: g.string().optional(),
 });
 
 export default config({
