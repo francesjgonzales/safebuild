165fc20d92cbb3f983a95de9e1759ead1491e569
francesjgonzales <francesj7837@gmail.com>
2024-01-09 22:55:30 -0700

    fixing deployment issue of implicit 'any' type error

--- src/app/news/ArticleList.tsx
+++ src/app/news/ArticleList.tsx
@@ -1,5 +1,7 @@
 //This is a server component
 
+import { Key } from "react";
+
 async function getArticles() {
   const res = await fetch("http://localhost:4000/news");
   return res.json();
@@ -9,13 +11,20 @@ export default async function ArticleList() {
   const articles = await getArticles();
   return (
     <>
-      {articles.map((article) => (
-        <div key={article.id} className="card my-5">
-          <p className="py-10 subheader">{article.title}</p>
-          <p>{article.date}</p>
-          <p>{article.article}</p>
-        </div>
-      ))}
+      {articles.map(
+        (article: {
+          id: Key;
+          title: string;
+          date: number;
+          article: string;
+        }) => (
+          <div key={article.id} className="card my-5">
+            <p className="py-10 subheader">{article.title}</p>
+            <p>{article.date}</p>
+            <p>{article.article}</p>
+          </div>
+        )
+      )}
     </>
   );
 }
